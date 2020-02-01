0ec463d9414ef13a575177d222a4c33647ac30e3
hacusk <yusk9816@gmail.com>
2020-02-02 01:51:03 +0900

    コメント書かないとダメらしい

--- src/model/WeatherModel.go
+++ src/model/WeatherModel.go
@@ -1,30 +1,38 @@
 package model
 
+// Weather return Location and Forecasts Info
 type Weather struct {
 	Location  Location    `json:"location"`
 	Forecasts []Forecasts `json:"forecasts"`
 }
 
+// Location return Location Info
 type Location struct {
 	City       string `json:"city"`
 	Area       string `json:"area"`
 	Prefecture string `json:"prefecture"`
 }
 
+// Forecasts return Today and Tommorow Forecast Info
 type Forecasts struct {
+	DateLabel   string      `json:"dateLabel"`
+	Date        string      `json:"date"`
 	Telop       string      `json:"telop"`
 	Temperature Temperature `json:"temperature"`
 }
 
+// Temperature return Temperature Info
 type Temperature struct {
 	MinTemperature MinTemperature `json:"min"`
 	MaxTemperature MaxTemperature `json:"max"`
 }
 
+// MinTemperature return MinTemperature Celsius
 type MinTemperature struct {
 	Celsius string `json:"celsius"`
 }
 
+// MaxTemperature return MaxTemperature Celsius
 type MaxTemperature struct {
 	Celsius string `json:"celsius"`
 }
